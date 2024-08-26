32f1c550dc40709c553f6170ff8eedf4f1ad0134
Guillermo Gómez Fonfría <guillermo.gomez@ciemat.es>
2024-08-26 19:20:10 +0200

    Debugging: working version with both tw_* > 0.0
    *Computationally* working version but the model is yet to be tested

--- src/Applications/SolidFD.jl
+++ src/Applications/SolidFD.jl
@@ -74,7 +74,7 @@ function _SolidFD(;
   # ns elements in the direction of the solid domain.
   # 2*ns accounts for ns solid cells on each side of the liquid for each
   # direction
-  nc = nl .+ 2*ns
+  nc = nl .+ (2 .* ns)
 
   info = Dict{Symbol,Any}()
   params = Dict{Symbol,Any}(
@@ -120,9 +120,10 @@ function _SolidFD(;
   # Prepare problem in terms of reduced quantities
   _nc = (nc[1],nc[2],3)
   _rank_partition = (rank_partition[1], rank_partition[2], 1)
+  _mesh_map = mesh_map(Ha, b, tw_Ha, tw_s, nc, nl, ns, domain, stretch_fine)
   model = CartesianDiscreteModel(
     parts, _rank_partition, domain, _nc;
-    isperiodic=(false, false, true), map=meshmap
+    isperiodic=(false, false, true), map=_mesh_map
   )
   solidFD_add_tags!(model, b, tw_Ha, tw_s)
   Ω = Interior(model)
@@ -132,7 +133,7 @@ function _SolidFD(;
   end
 
   params[:model] = model
-  params[:fluid] = Dict(
+  params[:fluid] = Dict{Symbol, Any}(
     :domain=>nothing,
     :α=>α,
     :β=>β,
@@ -141,18 +142,18 @@ function _SolidFD(;
     :B=>dirB,
     :ζ=>0.0,
   )
-  if (tw_s > 0.0) & (tw_Ha > 0.0)
+  if (tw_s > 0.0) | (tw_Ha > 0.0)
     params[:fluid][:domain] = "fluid"
-    σ_Ω = σ_field(model, Ω)
+    σ_Ω = σ_field(model, Ω, cw_Ha, cw_s, tw_Ha, tw_s)
     params[:solid] = Dict(:domain=>"solid", :σ=>σ_Ω)
+  end
 
   insulated_tags, thinWall_params = wall_BC(cw_Ha, cw_s, tw_Ha, tw_s, τ_Ha, τ_s)
   params[:bcs] =  Dict(
     :u=>Dict(:tags=>["Ha_walls","side_walls"]),
-    :j=>insulated_tags,
-    :thin_wall=>thinWall_params
+    :j=>Dict(:tags=>insulated_tags),
+    :thin_wall=>thinWall_params,
   )
-  solidFD_add_tags!()
 
   toc!(t,"pre_process")
 
@@ -206,12 +207,19 @@ function _SolidFD(;
   dev_kp = 100*abs(kp_a-kp)/max(kp_a,kp)
 
   if vtk
-    writevtk(Ω,joinpath(path,title),
-      order=2,
-      cellfields=[
-        "uh"=>uh_n,"e_u"=>e_u,"ph"=>ph_n,"jh"=>jh_n,"phi"=>φh_n,"div_jh"=>div_jh_n,"div_uh"=>div_uh_n])
+    cellfields = Vector{Pair}([
+      "uh"=>uh_n,
+      "e_u"=>e_u,
+      "ph"=>ph_n,
+      "jh"=>jh_n,
+      "phi"=>φh_n,
+      "div_jh"=>div_jh_n,
+      "div_uh"=>div_uh_n,
+    ])
     if (tw_Ha > 0.0) & (tw_s > 0.0)
       push!(cellfields, "σ"=>σ_Ω)
+    end
+    writevtk(Ω, joinpath(path,title), order=2, cellfields=cellfields)
     toc!(t,"vtk")
   end
   if verbose
@@ -237,11 +245,12 @@ function _SolidFD(;
   info[:kp] = kp
   info[:kp_a] = kp_a
   info[:dev_kp] = dev_kp
-  info, t
+
+  return info, t
 end
 
 """
-  σ_field(model, Ω)
+  σ_field(model, Ω, cw_Ha::Real, cw_s::Real, tw_Ha::Real, tw_s::Real)
 
 Returns a DistributedCellField with the adequate conductivity for each domain
 region.
@@ -291,7 +300,7 @@ function σ_field(model, Ω, cw_Ha::Real, cw_s::Real, tw_Ha::Real, tw_s::Real)
     local_views(labels), local_views(cells), local_views(Ω)
   ) do labels, partition, trian
     cell_entity = labels.d_to_dface_to_entity[end]
-    σ_field(σ_Ha, σ_s, labels, trian, cell_entity[partition.own_to_local])
+    _σ_field(σ_l, σ_Ha, σ_s, labels, trian, cell_entity[partition.own_to_local])
   end
   return GridapDistributed.DistributedCellField(σ_f, Ω)
 end
@@ -304,27 +313,36 @@ Assign tags to entities for every possible combination of tw_Ha and tw_s.
 In the process, it identifies the mesh elements corresponding to the solid
 region.
 """
+function solidFD_add_tags!(
+  model::GridapDistributed.DistributedDiscreteModel,
+  b::Real,
+  tw_Ha::Real,
+  tw_s::Real,
+)
+  map(local_views(model)) do model
+    solidFD_add_tags!(model, b, tw_Ha, tw_s)
+  end
+
+  return nothing
+end
+
 function solidFD_add_tags!(model, b::Real, tw_Ha::Real, tw_s::Real)
   labels = get_face_labeling(model)
   tags_outlet = append!(collect(1:20),[22])
-  add_tag_from_tags!(labels,"outlet",tags_outlet)
-  if (tw_Ha == 0.0) & (tw_s == 0.0)
-    tags_j_Ha = append!(collect(1:20),[23,24])
-    add_tag_from_tags!(labels,"Ha_walls",tags_j_Ha)
-    tags_j_side = append!(collect(1:20),[25,26])
-    add_tag_from_tags!(labels,"side_walls",tags_j_side)
-  else
+  tags_j_Ha = append!(collect(1:20), [23,24])
+  tags_j_side = append!(collect(1:20), [25,26])
+  add_tag_from_tags!(labels, "outlet", tags_outlet)
+  add_tag_from_tags!(labels, "Ha_walls", tags_j_Ha)
+  add_tag_from_tags!(labels, "side_walls", tags_j_side)
+
+  if (tw_Ha > 0.0) | (tw_s > 0.0)
     cell_entity = get_cell_entity(labels)
     # Label numbering
     if (tw_Ha > 0.0) & (tw_s == 0.0)
       solid_Ha = maximum(cell_entity) + 1
       solid_s = nothing
       fluid = solid_Ha + 1
-      tags_j_side = append!(collect(1:20),[25,26])
-      add_tag_from_tags!(labels,"side_walls",tags_j_side)
     elseif (tw_s > 0.0) & (tw_Ha == 0.0)
-      tags_j_Ha = append!(collect(1:20),[23,24])
-      add_tag_from_tags!(labels,"Ha_walls",tags_j_Ha)
       solid_Ha = nothing
       solid_s = maximum(cell_entity) + 1
       fluid = solid_s + 1
@@ -349,7 +367,7 @@ function solidFD_add_tags!(model, b::Real, tw_Ha::Real, tw_s::Real)
     grid = get_grid(model)
     cell_coords = get_cell_coordinates(grid)
     copyto!(cell_entity, map(set_entities, cell_coords))
-    solid_entities = Vector()
+    solid_entities = Vector{Int}()
     if !isnothing(solid_Ha)
       add_tag!(labels, "solid_Ha", [solid_Ha])
       push!(solid_entities, solid_Ha)
@@ -362,8 +380,11 @@ function solidFD_add_tags!(model, b::Real, tw_Ha::Real, tw_s::Real)
     add_tag!(labels,"fluid", [fluid])
     tags_j = vcat(collect(1:(8+12)), collect((1:4).+(8+12+2)))
     add_tag_from_tags!(labels, "insulating", tags_j)
-    add_non_slip_at_solid_entity!(model, solid_entities..., fluid, noslip)
+    Meshers.add_non_slip_at_solid_entity!(model, solid_entities, fluid, noslip)
     add_tag!(labels, "noslip", [noslip])
+  end
+
+  return nothing
 end
 
 """
@@ -375,7 +396,7 @@ to cw and tw.
 function wall_BC(
   cw_Ha::Real, cw_s::Real, tw_Ha::Real, tw_s::Real, τ_Ha::Real, τ_s::Real
 )
-  function _wall_BC!(cw::Real, tw::Real, τ::Real, tag:String)
+  function _wall_BC!(cw::Real, tw::Real, τ::Real, tag::String)
     if (cw == 0.0) & (tw == 0.0)
       push!(insulated_tags, tag)
     elseif (cw > 0.0) & (tw == 0.0)
@@ -386,20 +407,18 @@ function wall_BC(
           :domain =>Boundary(model, tags=tag)
         )
       )
-    elseif (cw > 0.0) & (tw > 0.0)
-      # Solid coupling
-      error("Not yet implemented.")
-    else
+    elseif (cw == 0.0) & (tw > 0.0)
       error("Insulator wall BC (cw = 0) requires thin wall approximation (tw = 0)")
     end
+
     return nothing
   end
 
   insulated_tags = Vector{String}()
   thinWall_options = Vector{Dict{Symbol,Any}}()
 
-  _wall_BC!(cw_Ha, tw_Ha, "Ha_walls")
-  _wall_BC!(cw_s, tw_s, "side_walls")
+  _wall_BC!(cw_Ha, tw_Ha, τ_Ha, "Ha_walls")
+  _wall_BC!(cw_s, tw_s, τ_s, "side_walls")
 
   return insulated_tags, thinWall_options
 end
@@ -413,68 +432,76 @@ Map function to pass to CartesianDiscreteModel.
 Combines the solid mesh map and the liquid stretchMHD map as required depending
 on the input tw_s, tw_Ha values and the stretch_fine toggle.
 """
-function mesh_map(coord)
-  stretch_Ha = sqrt(Ha/(Ha-1))
-  stretch_side = sqrt(sqrt(Ha)/(sqrt(Ha)-1))
+function mesh_map(Ha, b, tw_Ha, tw_s, nc, nl, ns, domain, stretch_fine)
+  function _mesh_map(coord)
+    stretch_Ha = sqrt(Ha/(Ha-1))
+    stretch_side = sqrt(sqrt(Ha)/(sqrt(Ha)-1))
 
-  if (tw_s > 0.0) | (tw_Ha > 0.0)
-    ncoord = solidMap(ncoord, tw=(tw_s, tw_Ha), nc=nc, ns=ns, nl=nl, domain=domain)
+    if (tw_s > 0.0) | (tw_Ha > 0.0)
+      ncoord = solidMap(coord, tw_Ha, tw_s, nc, ns, nl, domain)
+    end
+
+    if stretch_fine
+      ncoord = stretchMHD(coord,domain=(0,-b,0,-1.0),factor=(stretch_Ha,stretch_Ha),dirs=(1,2))
+      ncoord = stretchMHD(ncoord,domain=(0,b,0,1.0),factor=(stretch_Ha,stretch_Ha),dirs=(1,2))
+    else
+      ncoord = stretchMHD(coord,domain=(0,-b,0,-1.0),factor=(stretch_side,stretch_Ha),dirs=(1,2))
+      ncoord = stretchMHD(ncoord,domain=(0,b,0,1.0),factor=(stretch_side,stretch_Ha),dirs=(1,2))
+    end
+
+    return ncoord
   end
 
-  if stretch_fine
-    ncoord = stretchMHD(coord,domain=(0,-b,0,-1.0),factor=(stretch_Ha,stretch_Ha),dirs=(1,2))
-    ncoord = stretchMHD(ncoord,domain=(0,b,0,1.0),factor=(stretch_Ha,stretch_Ha),dirs=(1,2))
-  else
-    ncoord = stretchMHD(coord,domain=(0,-b,0,-1.0),factor=(stretch_side,stretch_Ha),dirs=(1,2))
-    ncoord = stretchMHD(ncoord,domain=(0,b,0,1.0),factor=(stretch_side,stretch_Ha),dirs=(1,2))
-  end
-  ncoord
+  return _mesh_map
 end
 
 """
-  solidMap(coord, tw, nc, ns, nl, domain)
+  solidMap(coord, tw_Ha, tw_s, nc, ns, nl, domain)
 
 Map to space out the mesh nodes evenly in the solid region according to the
 specified number of solid cells, ns, and the solid width, tw.  It also requires
 the total number of cells, nc, the number of cells in the liquid region, nl, and
 the domain.
 
-tw, nc, ns, and nl are assumed to be arrays corresponding to the X and Y
-directions respectevely.
+tw_s and tw_Ha are the side wall and Hartmann wall respective thickness, i.e.,
+they are the solid width in the X and Y directions, respectively.
+
+nc, ns, and nl are assumed to be arrays corresponding to the X and Y directions
+respectevely.
 
 domain is an array specifying the domain limits:
   domain = (x0, xf, y0, yf,...)
 """
-function solidMap(coord, tw, nc, ns, nl, domain)
+function solidMap(coord, tw_Ha, tw_s, nc, ns, nl, domain)
   ncoord = collect(coord.data)
   x0 = domain[1]
   xf = domain[2]
   y0 = domain[3]
   yf = domain[4]
   dx = (xf - x0)/nc[1]
-  dxl = (xf - x0 - 2*tw[1])/nl[1]
-  dxs = tw[1]/ns[1]
+  dxl = (xf - x0 - 2*tw_s)/nl[1]
+  dxs = tw_s/ns[1]
   dy = (yf - y0)/nc[2]
-  dyl = (yf - y0 - 2*tw[2])/nl[2]
-  dys = tw[2]/ns[2]
+  dyl = (yf - y0 - 2*tw_Ha)/nl[2]
+  dys = tw_Ha/ns[2]
 
   if tw_s > 0.0
     nx = abs(ncoord[1] - x0)/dx
     if nx < ns[1]
       ncoord[1] = x0 + nx*dxs
     elseif ns[1] <= nx <= (nl[1] + ns[1])
-      ncoord[1] = x0 + tw[1] + (nx - ns[1])*dxl
+      ncoord[1] = x0 + tw_s + (nx - ns[1])*dxl
     elseif nx > (nl[1] + ns[1])
-      ncoord[1] = x0 + tw[1] + nl[1]*dxl + (nx - nl[1] - ns[1])*dxs
+      ncoord[1] = x0 + tw_s + nl[1]*dxl + (nx - nl[1] - ns[1])*dxs
     end
   elseif tw_Ha > 0.0
     ny = abs(ncoord[2] - y0)/dy
     if ny < ns[2]
       ncoord[2] = y0 + ny*dys
     elseif ns[2] <= ny <= (nl[2] + ns[2])
-      ncoord[2] = y0 + tw[2] + (ny - ns[2])*dyl
+      ncoord[2] = y0 + tw_Ha + (ny - ns[2])*dyl
     elseif ny > (nl[2] + ns[2])
-      ncoord[2] = y0 + tw[2] + nl[2]*dyl + (ny - nl[2] - ns[2])*dys
+      ncoord[2] = y0 + tw_Ha + nl[2]*dyl + (ny - nl[2] - ns[2])*dys
     end
   end
   return VectorValue(ncoord)
